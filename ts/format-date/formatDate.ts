8c30118305a47b215a9b739bba83b9643de5a8de
Gideon Onyegbula <gigi.onyegbula.2007@gmail.com>
2026-04-06 19:27:17 +0100

    bug: resolved improper validation

--- ts/format-date/formatDate.ts
+++ ts/format-date/formatDate.ts
@@ -1,10 +1,9 @@
 // THIS FUNC FORMATS DATE FROM ISO TO MM/DD/YYYY
-export const formatDate = (isoString: string) => {
+export default function formatDate(isoString: string): string {
     if (!isoString) return '—';
 
-    const d = new Date(isoString);
-    const sampleNumber = 5;
-    if (typeof d !== typeof sampleNumber) return 'Invalid date';
+    const d = new Date(isoString);    
+    if (typeof d !== 'object') return 'Invalid date';
 
     return d.toLocaleString('en-GB', {
         day: '2-digit',
